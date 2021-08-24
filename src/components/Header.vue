d79787632ba5987a81763578b17b17887d086462
Your Name <muhthoriq73@gmail.com>
2021-08-24 10:08:08 +0700

    setup and config

---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="p-4 bg-blue-500">
+    <h1 class="text-center text-xl font-medium text-white">E-ATTENDANCE</h1>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
