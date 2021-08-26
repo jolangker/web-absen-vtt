61ca0a6cce35a5a7fedd51aa3f36583c6c10a72b
Your Name <muhthoriq73@gmail.com>
2021-08-26 08:47:59 +0700

    make admin page

---
+++ src/components/AttendanceInfo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="
+      px-6
+      py-4
+      bg-white
+      flex
+      justify-between
+      items-center
+      shadow
+      border-l-8 border-blue-700
+      rounded
+    "
+  >
+    <h2 class="text-2xl font-medium">
+      <slot name="title"></slot>
+    </h2>
+    <p class="font-orbitron font-medium text-6xl text-blue-400">
+      <slot name="value"></slot>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,7 +1,29 @@
-<template>Admin</template>
+<template>
+  <main class="px-16 py-6">
+    <date-time />
+    <div class="mt-5 flex flex-col space-y-8">
+      <attendance-info>
+        <template v-slot:title> Jumlah Siswa </template>
+        <template v-slot:value> 30 </template>
+      </attendance-info>
+      <attendance-info>
+        <template v-slot:title> Siswa Yang Sudah Absen </template>
+        <template v-slot:value> 27 </template>
+      </attendance-info>
+      <attendance-info>
+        <template v-slot:title> Siswa Yang Belum Absen </template>
+        <template v-slot:value> 3 </template>
+      </attendance-info>
+    </div>
+  </main>
+</template>
 
 <script>
-export default {};
+import AttendanceInfo from "../components/AttendanceInfo.vue";
+import DateTime from "../components/DateTime.vue";
+export default {
+  components: { AttendanceInfo, DateTime },
+};
 </script>
 
 <style></style>
