9235a7a7f951cef3f32586bd76a1a964ce66e6f5
Your Name <muhthoriq73@gmail.com>
2021-08-31 15:05:43 +0700

    change absenvttv3

--- src/composables/getToken.js
+++ src/composables/getToken.js
@@ -1,7 +1,7 @@
 import { ref } from "vue";
 
 const getToken = async () => {
-  const url = "https://absenvttv2.herokuapp.com/api/token/";
+  const url = "https://absenvttv3.herokuapp.com/api/token/";
   const cors = "https://cors-anywhere.herokuapp.com/";
 
   const res = await fetch(`${cors}${url}`, {
--- src/composables/getVariables.js
+++ src/composables/getVariables.js
@@ -1,6 +1,6 @@
 const getVariables = () => {
-  const urlSiswa = "https://absenvttv2.herokuapp.com/api/siswa/";
-  const urlAbsensi = "https://absenvttv2.herokuapp.com/api/absensi/";
+  const urlSiswa = "https://absenvttv3.herokuapp.com/api/siswa/";
+  const urlAbsensi = "https://absenvttv3.herokuapp.com/api/absensi/";
   const cors = "https://cors-anywhere.herokuapp.com/";
   const retToken = sessionStorage.getItem("token");
 
