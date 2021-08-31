1ee3bf4d10ebca42172f75623f745f18f1ab2a47
Your Name <muhthoriq73@gmail.com>
2021-08-31 15:14:48 +0700

    fix purge css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: {
-    content: ["./index.html", "./src/**/.*{vue,js,ts,jsx,tsx}"],
+    content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
     safelist: ["hidden", "ring-red-400", " cursor-not-allowed", " opacity-50"],
   },
   darkMode: false, // or 'media' or 'class'
