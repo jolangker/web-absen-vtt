28cf0725ad51cca277ae1b123108d9d4e3ec709c
Your Name <muhthoriq73@gmail.com>
2021-09-13 11:12:28 +0700

    custom scrllbar

--- src/App.vue
+++ src/App.vue
@@ -8,4 +8,27 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+::-webkit-scrollbar {
+  width: 8px;
+  /* height: 16px; */
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  border-radius: 100vh;
+  background: transparent;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #93c5fd;
+  border-radius: 100vh;
+  /* border: 3px solid #edf2f7; */
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #3b82f6;
+}
+</style>
